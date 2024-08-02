887cf7c19d3ab320678ee339185eee73acb10155
rohitjaiswal-3878 <rj710910@gmail.com>
2024-08-02 22:07:56 +0530

    Created profile details section.

--- src/components/ProfileDetials/ProfileDetails.jsx
+++ src/components/ProfileDetials/ProfileDetails.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./index.css";
+import profileIcon from "../../assets/Avatar.png";
 
 function ProfileDetails() {
-  return <div>ProfileDetails</div>;
+  return (
+    <div className="profile-details">
+      <div className="profile-details-container">
+        <div className="profile-image">
+          <img src={profileIcon} alt="profile image" />
+        </div>
+        <h1>
+          I do code and <br />
+          make content about it!
+        </h1>
+        <p>
+          I am a seasoned full-stack software engineer with over <br /> 8 years
+          of professional experience, specializing in backend development.
+          <br /> My expertise lies in crafting robust and scalable SaaS-based
+          <br />
+          architectures on the Amazon AWS platform.
+        </p>
+        <div className="profile-buttons">
+          <button className="btn-1">Get in touch</button>
+          <button className="btn-2">Download CV</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileDetails;
