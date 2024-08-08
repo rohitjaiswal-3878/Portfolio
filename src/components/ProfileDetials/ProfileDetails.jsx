0308e0f84e3ba3a5a86d0f36a9c72d97a0c9163a
rohitjaiswal-3878 <rj710910@gmail.com>
2024-08-08 20:14:29 +0530

    Done with getting profile details from backend to frontend.

--- src/components/ProfileDetials/ProfileDetails.jsx
+++ src/components/ProfileDetials/ProfileDetails.jsx
@@ -11,36 +11,48 @@ function ProfileDetails() {
     resume: "",
   });
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     getProfile();
   }, []);
 
   const getProfile = async () => {
-    const data = await getDetails();
-    console.log(data);
+    try {
+      const response = await getDetails();
+      setDetails({
+        profileImage: response.data[0].profileImage,
+        title: response.data[0].title,
+        description: response.data[0].description,
+        resume: response.data[0].resumeFile,
+      });
+      setLoading(!loading);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <div className="profile-details">
-      <div className="profile-details-container">
-        <div className="profile-image">
-          <img src={profileIcon} alt="profile image" />
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="profile-details-container">
+          <div className="profile-image">
+            <img src={details.profileImage} alt="profile image" />
+          </div>
+          <h1>{details.title}</h1>
+          <p>{details.description}</p>
+          <div className="profile-buttons">
+            <a href="">
+              <div className="btn-1 btn">Get in touch</div>
+            </a>
+            <a href={details.resume} download="Resume.pdf">
+              <div className="btn-2 btn">Download CV</div>
+            </a>
+          </div>
         </div>
-        <h1>
-          I do code and <br />
-          make content about it!
-        </h1>
-        <p>
-          I am a seasoned full-stack software engineer with over 8 years of
-          professional experience, specializing in backend development. My
-          expertise lies in crafting robust and scalable SaaS-based
-          architectures on the Amazon AWS platform.
-        </p>
-        <div className="profile-buttons">
-          <button className="btn-1">Get in touch</button>
-          <button className="btn-2">Download CV</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
