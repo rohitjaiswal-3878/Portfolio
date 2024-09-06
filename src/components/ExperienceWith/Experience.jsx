7053912a581e7bcc9d9303e35d0c946cafeb5046
rohitjaiswal-3878 <rj710910@gmail.com>
2024-09-06 21:29:30 +0530

    Fixed minor issues.

--- src/components/ExperienceWith/Experience.jsx
+++ src/components/ExperienceWith/Experience.jsx
@@ -3,16 +3,54 @@ import "./index.css";
 import { getSkills } from "../../apis/details";
 
 function Experience() {
-  const [skills, setSkills] = useState([]);
-
-  const fetchSkills = async () => {
-    const data = await getSkills();
-    setSkills(data);
-  };
-
-  useState(() => {
-    fetchSkills();
-  }, []);
+  const [skills, setSkills] = useState([
+    {
+      iconURL:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/255px-HTML5_logo_and_wordmark.svg.png",
+      title: "HTML",
+    },
+    {
+      iconURL:
+        "https://www.google.com/imgres?q=CSS%20icon&imgurl=https%3A%2F%2Fstatic-00.iconduck.com%2Fassets.00%2Fcss-3-icon-726x1024-610441pl.png&imgrefurl=https%3A%2F%2Ficonduck.com%2Ficons%2F94188%2Fcss-3&docid=6v7-6hDbT2tKpM&tbnid=5ml0sTq67lhHOM&vet=12ahUKEwjuw__91a6IAxXz6jgGHdy_N3cQM3oECGkQAA..i&w=726&h=1024&hcb=2&ved=2ahUKEwjuw__91a6IAxXz6jgGHdy_N3cQM3oECGkQAA",
+      title: "CSS",
+    },
+    {
+      iconURL: "",
+      title: "JavaScript",
+    },
+    {
+      iconURL: "",
+      title: "React",
+    },
+    {
+      iconURL: "",
+      title: "Node JS",
+    },
+    {
+      iconURL: "",
+      title: "MongoDB",
+    },
+    {
+      iconURL: "",
+      title: "Express JS",
+    },
+    {
+      iconURL: "",
+      title: "Java",
+    },
+    {
+      iconURL: "",
+      title: "MySql",
+    },
+    {
+      iconURL: "",
+      title: "Git",
+    },
+    {
+      iconURL: "",
+      title: "Github",
+    },
+  ]);
 
   return (
     <div className="experience">
--- src/components/ProfileDetials/ProfileDetails.jsx
+++ src/components/ProfileDetials/ProfileDetails.jsx
@@ -1,58 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
-import { getDetails } from "../../apis/details";
+import profileImg from "../../assets/profileIcon.jpeg";
+import resume from "../../assets/Resume.pdf";
+
 import { Link } from "react-router-dom";
 
 function ProfileDetails() {
   const [details, setDetails] = useState({
-    profileImage: "",
-    title: "",
-    description: "",
-    resume: "",
+    profileImage: profileImg,
+    title: "Hey I'm Rohit Jaiswal. A Full stack developer based in mumbai",
+    description:
+      "With a year of hands-on experience in web development, I specialize in the MERN stack. I combine creativity with technical skills to solve problems and build effective apps. Need help? Just send me a message!",
+    resume: resume,
   });
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    getProfile();
-  }, []);
-
-  const getProfile = async () => {
-    try {
-      const response = await getDetails();
-      setDetails({
-        profileImage: response.data[0].profileImage,
-        title: response.data[0].title,
-        description: response.data[0].description,
-        resume: response.data[0].resumeFile,
-      });
-      setLoading(!loading);
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <div className="profile-details">
-      {loading ? (
-        <div className="loader"></div>
-      ) : (
-        <div className="profile-details-container">
-          <div className="profile-image">
-            <img src={details.profileImage} alt="profile image" />
-          </div>
-          <h1>{details.title}</h1>
-          <p>{details.description}</p>
-          <div className="profile-buttons">
-            <Link to="/contact">
-              <div className="btn-1 btn">Get in touch</div>
-            </Link>
-            <a href={details.resume} download="Resume.pdf">
-              <div className="btn-2 btn">Download CV</div>
-            </a>
-          </div>
+      <div className="profile-details-container">
+        <div className="profile-image">
+          <img src={details.profileImage} alt="profile image" />
         </div>
-      )}
+        <h1>{details.title}</h1>
+        <p>{details.description}</p>
+        <div className="profile-buttons">
+          <Link to="/contact">
+            <div className="btn-1 btn">Get in touch</div>
+          </Link>
+          <a href={details.resume} download="Resume.pdf">
+            <div className="btn-2 btn">Download CV</div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
