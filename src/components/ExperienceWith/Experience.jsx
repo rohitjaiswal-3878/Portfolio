14cbe2f0b590f4b35b4d6f5e4dd3cb7ba7ba1f68
rohitjaiswal-3878 <rj710910@gmail.com>
2024-09-08 22:32:22 +0530

    Changed the skills fetching part.

--- src/components/ExperienceWith/Experience.jsx
+++ src/components/ExperienceWith/Experience.jsx
@@ -1,18 +1,64 @@
 import React, { useState } from "react";
 import "./index.css";
-import { getSkills } from "../../apis/details";
+import htmlIcon from "../../assets/Skills/html.png";
+import cssIcon from "../../assets/Skills/css.png";
+import jsIcon from "../../assets/Skills/js.png";
+import reactIcon from "../../assets/Skills/react.png";
+import expressIcon from "../../assets/Skills/express.png";
+import mongoIcon from "../../assets/Skills/mongo.png";
+import nodeIcon from "../../assets/Skills/node.png";
+import gitIcon from "../../assets/Skills/git.png";
+import githubIcon from "../../assets/Skills/github.png";
+import mysqlIcon from "../../assets/Skills/mysql.png";
+import javaIcon from "../../assets/Skills/java.png";
 
 function Experience() {
-  const [skills, setSkills] = useState([]);
-
-  const fetchSkills = async () => {
-    const data = await getSkills();
-    setSkills(data);
-  };
-
-  useState(() => {
-    fetchSkills();
-  }, []);
+  const [skills, setSkills] = useState([
+    {
+      iconURL: htmlIcon,
+      title: "HTML",
+    },
+    {
+      iconURL: cssIcon,
+      title: "CSS",
+    },
+    {
+      iconURL: jsIcon,
+      title: "JS",
+    },
+    {
+      iconURL: reactIcon,
+      title: "React",
+    },
+    {
+      iconURL: expressIcon,
+      title: "Express",
+    },
+    {
+      iconURL: mongoIcon,
+      title: "Mongo",
+    },
+    {
+      iconURL: nodeIcon,
+      title: "Node",
+    },
+    {
+      iconURL: gitIcon,
+      title: "Git",
+    },
+    {
+      iconURL: githubIcon,
+      title: "Github",
+    },
+    {
+      iconURL: mysqlIcon,
+      title: "MySql",
+    },
+    {
+      iconURL: javaIcon,
+      title: "Java",
+    },
+  ]);
 
   return (
     <div className="experience">
