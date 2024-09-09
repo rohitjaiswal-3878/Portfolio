a537a80a379adc38e58689290b877db2ca97e9c4
rohitjaiswal-3878 <rj710910@gmail.com>
2024-09-09 22:58:12 +0530

    Fixed minor issues

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,48 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./index.css";
-import projectThumbnail1 from "../../assets/thumb1.png";
-import projectThumbnail2 from "../../assets/thumb2.png";
+import quizzie from "../../assets/Projects/t1.png";
+import notesTaking from "../../assets/Projects/t2.png";
+import findJob from "../../assets/Projects/t3.png";
+
 import { MdArrowOutward } from "react-icons/md";
-import { getProjects } from "../../apis/details";
 
 function Projects() {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchProjects = async () => {
-    const data = await getProjects();
-    setProjects(data);
-    setLoading(!loading);
-  };
-
-  useEffect(() => {
-    fetchProjects();
-  }, []);
+  const [projects, setProjects] = useState([
+    {
+      thumbnail: quizzie,
+      url: "https://quiz-app-rohit-jaiswal.vercel.app/",
+      title: "Quizzie",
+    },
+    {
+      thumbnail: notesTaking,
+      url: "https://react-notes-taking-app.vercel.app/",
+      title: "Notes Taking App",
+    },
+    {
+      thumbnail: findJob,
+      url: "https://669a748abe9d4f65253f3583--rohitjaiswal-find-jobs.netlify.app/",
+      title: "Find Job",
+    },
+  ]);
 
   return (
     <div className="projects">
       <div className="projects-container">
         <h1>projects</h1>
-        {loading ? (
-          <div className="loader" style={{ marginTop: "20px" }}></div>
-        ) : (
-          <div className="projects-data">
-            {projects.map((project, index) => (
-              <div className="project" key={index}>
-                <img src={project.thumbnail} alt="thumbail" />
-                <a href={project.url} target="_blank">
-                  <div className="project-details">
-                    <div>
-                      <span>Click here to visit</span>
-                      <p>{project.title}</p>
-                    </div>
-                    <MdArrowOutward />
+
+        <div className="projects-data">
+          {projects.map((project, index) => (
+            <div className="project" key={index}>
+              <img src={project.thumbnail} alt="thumbail" />
+              <a href={project.url} target="_blank">
+                <div className="project-details">
+                  <div>
+                    <span>Click here to visit</span>
+                    <p>{project.title}</p>
                   </div>
-                </a>
-              </div>
-            ))}
-          </div>
-        )}
+                  <MdArrowOutward />
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
